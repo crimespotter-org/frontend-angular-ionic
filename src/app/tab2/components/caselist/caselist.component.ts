cde0bc1e362798311c9c087a1a4555c99d29733e
Hannes Kollert <hako94@web.de>
2024-04-04 17:39:02 +0200

    add menu for case list

--- src/app/tab2/components/caselist/caselist.component.ts
+++ src/app/tab2/components/caselist/caselist.component.ts
@@ -1,15 +1,41 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {
-  IonContent,
+  IonButton,
+  IonButtons, IonChip,
+  IonContent, IonDatetime,
   IonFab,
   IonFabButton,
-  IonFabList, IonHeader,
+  IonFabList,
+  IonHeader,
   IonIcon,
   IonItem,
+  IonItemOption,
+  IonItemOptions,
+  IonItemSliding,
   IonLabel,
-  IonList, IonSearchbar, IonTitle, IonToolbar
+  IonList, IonMenu,
+  IonSearchbar, IonSelect, IonSelectOption,
+  IonTitle,
+  IonToolbar, MenuController
 } from "@ionic/angular/standalone";
 import {NgForOf} from "@angular/common";
+import {addIcons} from "ionicons";
+import {
+  add,
+  calendar, closeCircle,
+  createOutline,
+  imagesOutline,
+  layers,
+  locate,
+  micCircleOutline,
+  optionsOutline,
+  newspaperOutline,
+  pin,
+  pricetag,
+  thumbsDownOutline,
+  thumbsUpOutline
+} from "ionicons/icons";
+
 
 @Component({
   selector: 'app-caselist',
@@ -28,19 +54,61 @@ import {NgForOf} from "@angular/common";
     IonTitle,
     IonToolbar,
     IonSearchbar,
-    NgForOf
+    NgForOf,
+    IonItemOption,
+    IonItemOptions,
+    IonItemSliding,
+    IonButtons,
+    IonButton,
+    IonDatetime,
+    IonMenu,
+    IonChip,
+    IonSelect,
+    IonSelectOption
 
   ],
   standalone: true
 })
-export class CaselistComponent   {
+export class CaselistComponent {
 
-  constructor() { }
+  constructor(private menu: MenuController) {
+    addIcons({
+      thumbsUpOutline,
+      thumbsDownOutline,
+      micCircleOutline,
+      newspaperOutline,
+      imagesOutline,
+      closeCircle,
+      createOutline,
+      calendar,
+      layers,
+      optionsOutline,
+      pricetag,
+      locate,
+      pin,
+      add
+    });
+  }
 
 
   faelle = [
-    { titel: 'Fall 1', zusammenfassung: 'Zusammenfassung von Fall 1' },
-    { titel: 'Fall 2', zusammenfassung: 'Zusammenfassung von Fall 2' },
+    {titel: 'Fall 1', zusammenfassung: 'Zusammenfassung von Fall 1'},
+    {titel: 'Fall 2', zusammenfassung: 'Zusammenfassung von Fall 2'},
   ];
 
+  openFilterMenu() {
+    this.menu.enable(true, 'filterMenu');
+    this.menu.open('filterMenu');
+  }
+
+  applyFilters() {
+
+    this.menu.close('filterMenu');
+  }
+
+  //removeFilter(filter) {
+    // Entfernen Sie den Filter aus der Liste der angewendeten Filter
+    //this.appliedFilters = this.appliedFilters.filter(f => f !== filter);
+    // Aktualisieren Sie Ihre Liste entsprechend
+  //}
 }
