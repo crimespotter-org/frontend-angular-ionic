8c7387783e12841097c3f3bf2fed49d08c645e00
julian <julian_2002@gmx.net>
2024-04-05 12:56:03 +0200

    add custom marker icons

--- src/app/tab1/components/map/map.component.ts
+++ src/app/tab1/components/map/map.component.ts
@@ -4,6 +4,7 @@ import { Location } from "../../../shared/interfaces/location.interface"
 import { SupabaseService } from "../../../services/supabase.service";
 import * as L from 'leaflet';
 import { Case } from 'src/app/shared/types/supabase';
+import { defaultMarker, murderMarker } from './markers';
 
 @Component({
   selector: 'app-map',
@@ -16,38 +17,33 @@ export class MapComponent implements AfterViewInit {
   private supabaseService: SupabaseService = inject(SupabaseService);
 
   constructor(){
-    this.defaultIcon = L.icon({
-        iconUrl: '../../assets/images/marker-icon.png',
-        shadowUrl: '../../assets/images/marker-shadow.png',
-        iconAnchor: [12.5, 41],
-        popupAnchor: [0, -40]
-    });
   }
 
-  private defaultIcon!: L.Icon;
   private map!: L.Map;
 
-  ngAfterViewInit(): void {
+  async ngAfterViewInit(): Promise<void> {
 
-    Geolocation.getCurrentPosition().then((position) => {
-      const coordinates = {
-        latitude: position.coords.latitude,
-        longitude: position.coords.longitude,
-      }
-      setTimeout(() => this.initMap(coordinates), 0);
-    }).catch((error) => {
-      console.log(error);
-      const coordinates = {
+    try{
+      const userPosition = await Geolocation.getCurrentPosition();
+
+      //current position of user 
+      this.initMap({
+        latitude: userPosition.coords.latitude, 
+        longitude: userPosition.coords.longitude});
+
+      L.marker([userPosition.coords.latitude, userPosition.coords.longitude], {icon: defaultMarker}).bindPopup("Hier sind Sie").addTo(this.map);
+    }
+    catch(error){
+      //Berlin
+      this.initMap({
         latitude: 52.5200,
         longitude: 13.4050
-      }
-      setTimeout(() => this.initMap(coordinates), 0);
-    }).then(() => {
-      this.supabaseService.getAllCases().then((data: Case[]) => {
-        data.forEach((item: Case) => this.addMarker(item))
-      });
+      })
     }
-    );
+    
+    this.supabaseService.getAllCases().then((data: Case[]) => {
+      data.forEach((item: Case) => this.addCaseMarker(item))
+    });
   };
 
 
@@ -57,11 +53,17 @@ export class MapComponent implements AfterViewInit {
       maxZoom: 19,
       attribution: '© OpenStreetMap'
     }).addTo(this.map);
-    L.marker([initialPosition.latitude, initialPosition.longitude], {icon: this.defaultIcon}).addTo(this.map);
+
+    this.map.on('zoomend', (event) => {
+      const currentZoomLevel = this.map.getZoom();
+      console.log("Current zoom level: " + currentZoomLevel);
+      // You can perform any actions based on the current zoom level here
+    });
   }
 
-  private addMarker(caze: Case){
-    L.marker([caze.lat, caze.long], {icon: this.defaultIcon}).bindPopup(`${caze.title} <br /> `).addTo(this.map);
+  private addCaseMarker(caze: Case){
+    //TODO: Distinguish between different case types. Seems do be not in db currently
+    L.marker([caze.lat, caze.long], {icon: murderMarker}).bindPopup(`${caze.title} <br /> `).addTo(this.map);
   }
 
 }
