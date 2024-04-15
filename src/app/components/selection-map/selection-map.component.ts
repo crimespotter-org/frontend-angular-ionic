de799059eba87940649195045002639262ca413e
julian <julian_2002@gmx.net>
2024-04-16 00:07:32 +0200

    allow user to also directly search coordinates in "google-maps-format"

--- src/app/components/selection-map/selection-map.component.ts
+++ src/app/components/selection-map/selection-map.component.ts
@@ -2,7 +2,7 @@ import { AfterViewInit, Component, EventEmitter, Input, OnInit, Output, ViewChil
 import * as L from 'leaflet';
 import { Location } from 'src/app/shared/interfaces/location.interface';
 import { defaultMarker } from 'src/app/components/tab1/components/map/markers';
-import {ViewDidEnter, IonSearchbar, IonItem, IonLabel, IonList } from '@ionic/angular/standalone';
+import { ViewDidEnter, IonSearchbar, IonItem, IonLabel, IonList } from '@ionic/angular/standalone';
 import { NominatimResponse } from 'src/app/shared/interfaces/nominatim-response';
 import { DataService } from 'src/app/services/data.service';
 import { CommonModule } from '@angular/common';
@@ -15,11 +15,11 @@ import { NgModel } from '@angular/forms';
   imports: [IonSearchbar, IonItem, IonLabel, IonList, CommonModule],
   standalone: true
 })
-export class SelectionMapComponent implements AfterViewInit{
+export class SelectionMapComponent implements AfterViewInit {
 
   dataService = inject(DataService);
 
-  @Input() location: Location = {latitude: 48.441976384366384, longitude: 8.684747075615647};
+  @Input() location: Location = { latitude: 48.441976384366384, longitude: 8.684747075615647 };
 
   @Output() selectedLocation = new EventEmitter<Location>();
 
@@ -29,15 +29,15 @@ export class SelectionMapComponent implements AfterViewInit{
   searchList: any[] = [];
 
   private map!: L.Map;
-  private marker: L.Marker = L.marker([0,0], {icon: defaultMarker, opacity: 0});
+  private marker: L.Marker = L.marker([0, 0], { icon: defaultMarker, opacity: 0 });
 
   constructor() { }
 
   ngAfterViewInit(): void {
-      setTimeout(() => this.initMap({latitude: this.location.latitude, longitude: this.location.longitude}), 0);
+    setTimeout(() => this.initMap({ latitude: this.location.latitude, longitude: this.location.longitude }), 0);
   }
 
-  initMap(coordinates: Location){
+  initMap(coordinates: Location) {
     if (this.map != undefined) this.map.remove();
     this.map = L.map('selectormap').setView([coordinates.latitude, coordinates.longitude], 13);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -47,7 +47,7 @@ export class SelectionMapComponent implements AfterViewInit{
 
     setTimeout(() => {
       this.map.invalidateSize();
-    },500);
+    }, 500);
 
     this.marker.addTo(this.map);
     this.map.on('click', this.onMapClick.bind(this));
@@ -62,22 +62,41 @@ export class SelectionMapComponent implements AfterViewInit{
     this.marker.setOpacity(1);
     this.marker.setLatLng([lat, lng]);
 
-    this.selectedLocation.emit({latitude: lat, longitude: lng});
+    this.selectedLocation.emit({ latitude: lat, longitude: lng });
   }
 
-  onSearchChange(event: any){
-    if(event == undefined || event.target == undefined || event.target.value === "") return;
+  onSearchChange(event: any) {
+    if (event == undefined || event.target == undefined || event.target.value === "") return;
     const searchText = event.target.value;
-    this.dataService.getLocationsNominatim(searchText).subscribe(results => {
-      this.searchList = results;
-      if(this.searchList.length == 1){
-        this.onSearchEntrySelected(this.searchList[0]);
-      }
-    });
+
+    const coordinatePattern = /^-?\d+(.\d+)?,\s*-?\d+(.\d+)?$/;
+    if (coordinatePattern.test(searchText)) {
+      console.log("Coordinates detected");
+      const [latitude, longitude] = searchText.split(',').map(Number);
+      console.log(latitude, longitude);
+      this.marker.setOpacity(1);
+      this.marker.setLatLng([latitude, longitude]);
+
+      this.selectedLocation.emit({ latitude: latitude, longitude: longitude });
+      this.onSearchEntrySelected({
+        latitude: latitude,
+        longitude: longitude,
+        postalCode: 0,
+        sub: '',
+        city: '',
+        county: ''
+      });
+    } else {
+      this.dataService.getLocationsNominatim(searchText).subscribe(results => {
+        this.searchList = results;
+        if (this.searchList.length == 1) {
+          this.onSearchEntrySelected(this.searchList[0]);
+        }
+      });
+    }
   }
 
-  onSearchEntrySelected(search: QueryLocationResponse){
-    console.log(search);
+  onSearchEntrySelected(search: QueryLocationResponse) {
     this.map.flyTo([search.latitude, search.longitude], 13, {
       animate: true,
       duration: 0.8
