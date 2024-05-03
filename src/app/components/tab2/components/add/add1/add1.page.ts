46f409e6d134232423316816548910b8f31aad32
i21023 <i21023@hb.dhbw-stuttgart.de>
2024-05-03 13:37:40 +0200

    nicer datetime selection in add event

--- src/app/components/tab2/components/add/add1/add1.page.ts
+++ src/app/components/tab2/components/add/add1/add1.page.ts
@@ -1,10 +1,10 @@
 import { AfterViewInit, Component, OnInit, ViewChild, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormGroup, FormsModule } from '@angular/forms';
+import { CommonModule, DatePipe } from '@angular/common';
+import { FormGroup, FormsModule, NgModel } from '@angular/forms';
 import { StorageService } from 'src/app/services/storage.service';
 import { addOutline, chevronForwardOutline, locationOutline, lockClosedOutline, lockOpenOutline, trashOutline, warningOutline } from 'ionicons/icons';
 import { addIcons } from 'ionicons';
-import { IonCard, IonCardContent, IonText, IonRow, IonCol, IonGrid, IonChip, IonInput, IonToast, IonItemSliding, IonItemOption, IonItemOptions, IonDatetime ,IonButton, IonButtons, IonHeader, IonContent, IonToolbar, IonLabel, IonTitle, IonItem, IonFab, IonIcon, IonFabButton, IonSelectOption, IonSelect, IonTextarea } from '@ionic/angular/standalone';
+import { IonModal, IonCard, IonCardContent, IonText, IonRow, IonCol, IonGrid, IonChip, IonInput, IonToast, IonItemSliding, IonItemOption, IonItemOptions, IonDatetime ,IonButton, IonButtons, IonHeader, IonContent, IonToolbar, IonLabel, IonTitle, IonItem, IonFab, IonIcon, IonFabButton, IonSelectOption, IonSelect, IonTextarea } from '@ionic/angular/standalone';
 import { Router } from '@angular/router';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { AddService } from 'src/app/services/add.service';
@@ -46,6 +46,9 @@ import { HelperUtils } from 'src/app/shared/helperutils';
       IonText,
       IonCardContent,
       IonCard,
+      IonModal,
+      DatePipe,
+      CommonModule
     ]
 })
 export class Add1Page implements AfterViewInit {
@@ -61,6 +64,7 @@ export class Add1Page implements AfterViewInit {
 
   form: FormGroup;
 
+  crimeDate?: Date;
   title: string | undefined;
   summary: string | undefined;
   type: string | undefined;
@@ -79,6 +83,7 @@ export class Add1Page implements AfterViewInit {
 
   routeToNextPage(){
     console.log(this.form.get('closed')?.value);
+console.log(this.form.value);
     if(this.form.valid){
       this.router.navigate(['tabs/tab2/add2']);
     }
